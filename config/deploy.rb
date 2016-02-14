1855e0d1a3cecfda9d5e454392b357533e094b68
poc7667 <poc7667@gmail.com>
2016-02-14 11:45:15 +0800

    add capitrano deploy files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,33 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
+binding.pry
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, ENV['APP_NAME']
+set :repo_url, ENV['GIT_REPO']
+set :format, :pretty
+set :keep_releases, 2
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# deploy 的資料夾位置 (prodution)
+set :deploy_to, ENV["PROJECT_PATH"]
+set :branch, ENV["BRANCH"] || `git rev-parse --abbrev-ref HEAD`.chop
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :user, ENV['DEPLOYER']
+set :use_sudo, false
+# rbenv 的設定
+set :rbenv_type, :user 
 
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :rbenv_ruby, "2.2.2"
+set :rbenv_path, ENV['RBENV_PATH']
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w(rake gem bundle ruby rails)
+set :rbenv_roles, :all
+set :rbenv_type, :user
+set :bundle_flags, "--deployment --verbose --binstubs --shebang ruby-local-exec"
+set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$HOME/bin:$HOME/local/bin:$PATH"
+}
 
 namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
 
 end
